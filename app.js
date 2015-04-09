e727817d63b53f29ad8d952c114e58db9c8ca4a0
Perry <perry_wu@gemteks.com>
2015-04-10 07:18:39 +0800

    first draft to have simple version

--- app.js
+++ app.js
@@ -5,8 +5,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var swig = require('swig');
-
+//var swig = require('swig');
 var app = express();
 
 var bodyParser = require('body-parser');
@@ -17,9 +16,17 @@ var methodOverride = require('method-override');
 var errorhandler = require('errorhandler');
 //var basicAuth = require('basic-auth');
 
-swig.setDefaults({ cache: false });
+var _ = require('lodash');
+var fs = require('fs');
+var util = require('util');
+var async = require('async');
+
+var dir = process.cwd();
+
+//swig.setDefaults({ cache: false });
+//app.engine('html', swig.renderFile);
+
 // all environments
-app.engine('html', swig.renderFile);
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
@@ -42,15 +49,72 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', function(req, res){
-	res.render('index', null);
-});
-
-app.get('/index.html', function(req, res){
-	res.render('index', null);
+	res.redirect('index.html');
 });
 
 app.get('/test.html', function(req, res){
-	res.render('test', null);
+	res.redirect('test.html');
+});
+
+app.get('/files', function(req, res) {
+	var currentDir = dir;
+	var query = req.query.path || '';
+	if (query) currentDir = path.join(dir, query);
+	console.log("browsing ", currentDir);
+
+	fs.readdir(currentDir, function(err, files) {
+		if (err) {
+			throw err;
+		}
+		var data = [];
+		var fileList = [];
+
+		files.filter(function(file) {
+			return true;
+		}).forEach(function(file) {
+			fileList.push({
+				file: file,
+				filePath: path.join(currentDir, file),
+				urlPath: path.join(query, file)
+			});
+		});
+
+		//console.log(fileList);
+		async.map(fileList, function(fileObj, cb) {
+			fs.stat(fileObj.filePath, cb);
+		},
+		function(err, results) {
+			if (err) {
+				console.log(err);
+				res.end("Error occurs");
+				return;
+			}
+			for (i = 0; i < results.length; i++) {
+				console.log("processing " + fileList[i].filePath + " " +
+				/*util.inspect(results[i]) +*/
+				" isDic :" + results[i].isDirectory());
+				if (results[i].isDirectory()) {
+					data.push({
+						Name: fileList[i].file,
+						IsDirectory: true,
+						Path: fileList[i].urlPath,
+						fileSize: results[i].size
+					});
+				} else {
+					var ext = path.extname(fileList[i].filePath);
+					data.push({
+						Name: fileList[i].file,
+						Ext: ext,
+						IsDirectory: false,
+						Path: fileList[i].urlPath,
+						fileSize: results[i].size
+					});
+				}
+			}
+			//data = _.sortBy(data, function(f) { return f.Name });
+			res.json(data);
+		});
+	});
 });
 
 http.createServer(app).listen(app.get('port'), function(){
---
+++ public/app.js
@@ -0,0 +1,240 @@
+(function($) {
+
+	function humanFileSize(bytes, si) {
+		var thresh = si ? 1000: 1024;
+		if (bytes < thresh) return bytes + ' B';
+		var units = si ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+		var u = - 1;
+		do {
+			bytes /= thresh; ++u;
+		} while (bytes >= thresh);
+		return bytes.toFixed(1) + ' ' + units[u];
+	};
+
+	var extensionsMap = {
+		".zip": "fa-file-archive-o",
+		".gz": "fa-file-archive-o",
+		".bz2": "fa-file-archive-o",
+		".xz": "fa-file-archive-o",
+		".rar": "fa-file-archive-o",
+		".tar": "fa-file-archive-o",
+		".tgz": "fa-file-archive-o",
+		".tbz2": "fa-file-archive-o",
+		".z": "fa-file-archive-o",
+		".7z": "fa-file-archive-o",
+		".mp3": "fa-file-audio-o",
+		".cs": "fa-file-code-o",
+		".c++": "fa-file-code-o",
+		".cpp": "fa-file-code-o",
+		".js": "fa-file-code-o",
+		".xls": "fa-file-excel-o",
+		".xlsx": "fa-file-excel-o",
+		".png": "fa-file-image-o",
+		".jpg": "fa-file-image-o",
+		".jpeg": "fa-file-image-o",
+		".gif": "fa-file-image-o",
+		".mpeg": "fa-file-movie-o",
+		".pdf": "fa-file-pdf-o",
+		".ppt": "fa-file-powerpoint-o",
+		".pptx": "fa-file-powerpoint-o",
+		".txt": "fa-file-text-o",
+		".log": "fa-file-text-o",
+		".doc": "fa-file-word-o",
+		".docx": "fa-file-word-o",
+	};
+
+	function getFileIcon(ext) {
+		return (ext && extensionsMap[ext.toLowerCase()]) || 'fa-file-o';
+	}
+
+	var currentPath = null;
+
+	$( function() {
+		$( "#popup-yn-page" ).enhanceWithin().popup();
+	});
+
+	$(document).ready(function(){
+		$("#pilla-b-home").on("click", function(e){
+			$.get("/files",function(data,status){
+				alert(data.length);
+				for(i = 0; i < data.length; i++) {
+					//alert(data[i].Name);
+					$("#pilla-list-main").append('<li data-icon="gear"><a href="#"><i class="fa fa-folder"></i>&nbsp;' + data[i].Name + '</a><a href="#itemPage">test</a></li>');
+				}
+				$("#pilla-list-main").listview('refresh');
+			});
+		});
+	});
+
+/*	
+ 		var options = {
+		"order": [[1, 'asc']],
+		"searching": false,
+		"processing": true,
+		"serverSide": false,
+		"paging": false,
+		"autoWidth": true,
+		//"scrollY":"400px",
+		"dom": '<"toolbar">frtip',
+		"createdRow": function(row, data, dataIndex) {
+			if (!data.IsDirectory) return;
+			var path = data.Path;
+			console.log(path);
+			$(row).on("click", "a", function(e) {
+				$.get('/files?path=' + path).then(function(fData) {
+					table.api().clear();
+					table.api().rows.add(fData).draw();
+					currentPath = path;
+				});
+				e.preventDefault();
+			});
+
+		},
+		"columns": [{
+			"targets": 0,
+			"data": null,
+			"orderable": false,
+			"className": "t-cell-check",
+			"defaultContent": "<input type='checkbox' class='t-chk-item' value=''>"
+		},
+		{
+			"targets": 1,
+			"data": null,
+			"className": "t-cell-file",
+			"render": function(data, type, row, meta) {
+				if (data.IsDirectory) {
+					return "<a href='#'><i class='fa fa-folder'></i>&nbsp;" + data.Name + "</a>";
+				} else {
+					return "<a href='/" + data.Path + "' target='_blank'><i class='fa " + getFileIcon(data.Ext) + "'></i>&nbsp;" + data.Name + "</a>";
+				}
+			}
+		},
+		{
+			"targets": 2,
+			"data": null,
+			"className": "t-cell-size",
+			//"width": "10%",
+			"render": function(data, type, row, meta) {
+				if (data.IsDirectory) return "<p>.</p>";
+				return "<p>" + humanFileSize(data.fileSize) + "</p>";
+			}
+		},
+		{
+			"targets": - 1,
+			"data": null,
+			"className": "t-cell-actions",
+			"orderable": false,
+			//"width": "20%",
+			"defaultContent": '<button type="button" class="btn btn-default btn-xs t-btn-info"><i class="fa fa-info"></i></button><button type="button" class="btn btn-default btn-xs t-btn-del"><i class="fa fa-trash-o"></i></button><button type="button" class="btn btn-default btn-xs t-btn-move"><i class="fa fa-arrow-right"></i></button><button type="button" class="btn btn-default btn-xs t-btn-copy"><i class="fa fa-copy"></i></button>'
+		}]
+	};
+
+	var table = $("#fileTable").dataTable(options);
+
+	$("div.toolbar").html('<div><b>File Browser</b> &nbsp;<div class="btn-group btn-group-xs" role="group" aria-label="what is this"><button type="button" class="btn btn-default" id="h-btn-up"><i class="fa fa-level-up"></i> Up </button></div><div class="btn-group btn-group-xs" role="group" aria-label="Is this for?"><button type="button" class="btn btn-default" id="h-btn-copy"><i class="fa fa-copy"></i> Copy </button><button type="button" class="btn btn-default" id="h-btn-move"><i class="fa fa-arrow-right"></i> Move </button><button type="button" class="btn btn-danger" id="h-btn-delete"><i class="fa fa-trash-o"></i> Delete </button><br /></div></div>');
+
+	$.get('/files').then(function(data) {
+		table.api().clear();
+		table.api().rows.add(data).draw();
+	});
+
+	// Request from table head 
+	$("#h-btn-up").on("click", function(e) {
+		if (!currentPath) return;
+		var idx = currentPath.lastIndexOf("/");
+		var path = currentPath.substr(0, idx);
+		$.get('/files?path=' + path).then(function(data) {
+			table.api().clear();
+			table.api().rows.add(data).draw();
+			currentPath = path;
+		});
+	});
+
+	$("#h-chk-items").on("click", function(e) {
+		var allRow$ = $("#fileTable tbody input.t-chk-item");
+		if ($(this).is(':checked')) {
+			console.log("checked");
+			allRow$.each(function() {
+				$(this).prop("checked", "checked");
+			});
+			table.api().draw();
+		} else {
+			console.log("no check");
+			allRow$.each(function() {
+				$(this).removeProp("checked", "checked");
+			});
+			table.api().draw();
+		}
+	});
+
+	$("#h-btn-copy").on("click", function(e) {
+		var selectedRow$ = $("#fileTable tbody input.t-chk-item:checked");
+		var count = 0;
+		selectedRow$.each(function() {
+			var td = $(this).closest('tr').children('td').next();
+			var cell = table.api().cell(td);
+			var data = cell.data();
+			console.log("copy " + count + "th data: " + data.Name);
+			count++;
+		});
+	});
+
+	$("#h-btn-move").on("click", function(e) {
+		var selectedRow$ = $("#fileTable tbody input.t-chk-item:checked");
+		var count = 0;
+		selectedRow$.each(function() {
+			var td = $(this).closest('tr').children('td').next();
+			var cell = table.api().cell(td);
+			var data = cell.data();
+			console.log("move " + count + "th data: " + data.Name);
+			count++;
+		});
+	});
+
+	$("#h-btn-delete").on("click", function(e) {
+		var selectedRow$ = $("#fileTable tbody input.t-chk-item:checked");
+		var count = 0;
+		selectedRow$.each(function() {
+			var td = $(this).closest('tr').children('td').next();
+			var cell = table.api().cell(td);
+			var data = cell.data();
+			console.log("delete " + count + "th data: " + data.Name);
+			count++;
+		});
+	});
+
+	// Request from table to do individual file action
+	$("#fileTable tbody").on("click", ".t-btn-info", function(e) {
+		var td = $(this).closest('tr').children('td').next();
+		var cell = table.api().cell(td);
+		var data = cell.data();
+
+		console.log("info: " + data.Name);
+	});
+
+	$("#fileTable tbody").on("click", ".t-btn-del", function(e) {
+		var td = $(this).closest('tr').children('td').next();
+		var cell = table.api().cell(td);
+		var data = cell.data();
+
+		console.log("del: " + data.Name);
+	});
+
+	$("#fileTable tbody").on("click", ".t-btn-move", function(e) {
+		var td = $(this).closest('tr').children('td').next();
+		var cell = table.api().cell(td);
+		var data = cell.data();
+
+		console.log("move: " + data.Name);
+	});
+
+	$("#fileTable tbody").on("click", ".t-btn-copy", function(e) {
+		var td = $(this).closest('tr').children('td').next();
+		var cell = table.api().cell(td);
+		var data = cell.data();
+
+		console.log("copy: " + data.Name);
+	});
+*/
+})(jQuery);
+
